bbeef349241b91fdac2fb91e9a5b7e1da09c0255
Gaurav Kukreja <gmkukreja@gmail.com>
2013-11-12 11:41:35 +0100

    Asgn2: Exer5: Pi calculcation serial and parallel code
    Signed-off-by: Gaurav Kukreja <gmkukreja@gmail.com>

---
+++ Asgn2/pi-calculation/omp_reduction_pi.c
@@ -0,0 +1,50 @@
+/**
+ * Assignment 2: Exercise 5:
+ * OpenMP Critical Pi Calculation
+ */
+
+#include <stdio.h>
+#include <math.h>
+#include <omp.h>
+#include "timer.h"
+
+int main(int argc, char **argv)
+{
+	double pi = 0.0;
+	double x = 0.0;
+	unsigned long steps = 1000000;
+	double dx= 0.0;
+	int i;
+	double val;
+	double time_taken;
+	time_marker_t time;
+
+	if(argc > 1)
+	{
+		steps = strtoul(argv[1], NULL, 0);
+	}
+	dx = 1.0 / steps;
+
+	printf("Step Size = %f\n", dx);	
+
+	time = get_time();
+
+	#pragma omp parallel for schedule(static) private(val) private(x) reduction(+:pi)
+	for(i = 0; i < steps; i++)
+	{
+		//printf("Num Threads = %d\n", omp_get_num_threads());
+		x = (i * dx) + (dx / 2);
+		val = (1.0 / (1.0 + x * x)) * dx;
+		pi += val;
+	}
+
+	pi = pi * 4;
+
+	time_taken = get_ToD_diff_time(time);
+
+	printf("Value of pi = %f\n", pi);
+	printf("Time Taken = %f\n", time_taken);
+
+	return 0;
+}
+
---
+++ Asgn2/pi-calculation/serial_pi.c
@@ -0,0 +1,45 @@
+/**
+ * Assignment 2: Exercise 5:
+ * Seial Pi Calculation
+ */
+
+#include <stdio.h>
+#include <math.h>
+#include "timer.h"
+
+int main(int argc, char **argv)
+{
+	double pi = 0.0;
+	double x = 0.0;
+	unsigned long steps = 1000000;
+	double dx= 0.0;
+	int i;
+	double time_taken;
+	time_marker_t time;
+
+	if(argc > 1)
+	{
+		steps = strtoul(argv[1], NULL, 0);
+	}
+	dx = 1.0 / steps;
+
+	printf("Step Size = %e\n", dx);
+
+	time = get_time();
+
+	for(i = 0; i < steps; i++)
+	{
+		x = (i * dx) + (dx / 2);
+		pi += (1.0 / (1.0 + x * x)) * dx;
+	}
+
+	pi = pi * 4;
+
+	time_taken = get_ToD_diff_time(time);
+
+	printf("Value of pi = %e\n", pi);
+	printf("Time Taken = %f\n", time_taken);
+
+	return 0;
+}
+
