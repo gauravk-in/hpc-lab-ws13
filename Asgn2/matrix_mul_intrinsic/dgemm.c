8e965efb10f42dd4e1213cf9aabd808ee2937f85
Gaurav Kukreja <gaurav.kukreja@tum.de>
2013-11-17 15:25:41 +0100

    Vector Intrinsic Matrix Multiplication - 7.2 GFlops
    Signed-off-by: Gaurav Kukreja <gaurav.kukreja@tum.de>

--- Asgn2/matrix_mul_intrinsic/dgemm.c
+++ Asgn2/matrix_mul_intrinsic/dgemm.c
@@ -9,7 +9,6 @@
 #include <string.h>
 #include <immintrin.h>
 
-
 #define BLOCKSIZE 1024
 
 int max(int a, int b){
@@ -22,8 +21,6 @@ inline int min(int a, int b){
         else return b;
 }
 
-//extern __m256d _mm256_loaddup_pd(double *);
-
 int main(int argc, char **argv)
 {
         int n;
@@ -33,7 +30,7 @@ int main(int argc, char **argv)
 
         long mem_size;
 
-        int bk, bj, i, j, k, l;
+        int bk, bj, i, j, k;
         int b_base, c_base;
         double r;
 	FILE *fp;
@@ -45,9 +42,9 @@ int main(int argc, char **argv)
         if(argc > 1){
                 n = atoi(argv[1]);
         }
-	if(argc > 2){
-		fp = fopen(argv[2], "w");
-	}	
+        if(argc > 2){
+                fp = fopen(argv[2], "w");
+        }
 
         //sprintf(logfile_name, "logfile_dgemm.txt");
         //logfile_handle = freopen(logfile_name, "w", stdout);
@@ -75,17 +72,35 @@ int main(int argc, char **argv)
 
         for(bk = 0; bk < n; bk+=BLOCKSIZE) {
                 for(bj = 0; bj < n; bj+=BLOCKSIZE) {
-                        for(i = 0; i < n; i++){
-                                for(k = bk, l = bk; k < min(n, bk+BLOCKSIZE); k++, l+=4){
-                                        //r = a[i * n + k];
-                                        __m256d a_r = _mm256_load_pd(&a[i * n + l]);
-                                        for(j = bj ; j < min(n, bj + BLOCKSIZE); j+=4){
-                                                __m256d c_r = _mm256_load_pd(&c[i * n + j]);
-                                                __m256d b_r = _mm256_load_pd(&b[k * n + j]);
-                                                c_r = _mm256_add_pd(c_r, _mm256_mul_pd(a_r, b_r));
-						_mm256_store_pd(&c[i * n + j], c_r);
-                                                //c[i * n + j] += r * b[k * n + j];
-                                        }
+                        for(i = 0; i < n; i+=2){
+                                for(k = bk; k < min(n, bk+BLOCKSIZE); k+=2){
+                                                        __m256d a1_r = _mm256_set_pd(a[i * n + k], a[i * n + k], a[i * n + k], a[i * n + k]);
+                                                        __m256d a2_r = _mm256_set_pd(a[i * n + k + 1], a[i * n + k + 1], a[i * n + k + 1], a[i * n + k + 1]);
+                                                        __m256d a3_r = _mm256_set_pd(a[(i+1) * n + k], a[(i+1) * n + k], a[(i+1) * n + k], a[(i+1) * n + k]);
+                                                        __m256d a4_r = _mm256_set_pd(a[(i+1) * n + k+1], a[(i+1) * n + k+1], a[(i+1) * n + k+1], a[(i+1) * n + k+1]);
+                                                        #pragma ivdep
+                                                        {
+                                                        for(j = bj ; j < min(n, bj + BLOCKSIZE); j+=8){
+                                                                __m256d c1_r = _mm256_load_pd(&c[i * n + j]);
+                                                                __m256d c2_r = _mm256_load_pd(&c[i * n + j + 4]);
+                                                                __m256d c3_r = _mm256_load_pd(&c[(i+1) * n + j]);
+                                                                __m256d c4_r = _mm256_load_pd(&c[(i+1) * n + j + 4]);
+                                                                __m256d b1_r = _mm256_load_pd(&b[k * n + j]);
+                                                                __m256d b2_r = _mm256_load_pd(&b[k * n + j + 4]);
+                                                                __m256d b3_r = _mm256_load_pd(&b[(k+1) * n + j]);
+                                                                __m256d b4_r = _mm256_load_pd(&b[(k+1) * n + j + 4]);
+                                                                c1_r = _mm256_add_pd(c1_r, _mm256_add_pd(_mm256_mul_pd(a1_r, b1_r), _mm256_mul_pd(a2_r, b3_r)));
+                                                                c2_r = _mm256_add_pd(c2_r, _mm256_add_pd(_mm256_mul_pd(a1_r, b2_r), _mm256_mul_pd(a2_r, b4_r)));
+                                                                c3_r = _mm256_add_pd(c3_r, _mm256_add_pd(_mm256_mul_pd(a3_r, b1_r), _mm256_mul_pd(a4_r, b3_r)));
+                                                                c4_r = _mm256_add_pd(c4_r, _mm256_add_pd(_mm256_mul_pd(a3_r, b2_r), _mm256_mul_pd(a4_r, b4_r)));
+                                                                _mm256_store_pd(&c[i * n + j], c1_r);
+                                                                _mm256_store_pd(&c[i * n + j + 4], c2_r);
+                                                                _mm256_store_pd(&c[(i+1) * n + j], c3_r);
+                                                                _mm256_store_pd(&c[(i+1) * n + j + 4], c4_r);
+                                                                //c[i * n + j] += r * b[k * n + j];
+                                                        }
+                                                        }
+
                                 }
                         }
                 }
@@ -95,12 +110,12 @@ int main(int argc, char **argv)
 
         print_flops(flops, time);
 
-	if(argc > 2)
-	{
-		for(i = 0; i < n; i++)
-			for(j = 0; j < n; j++)
-				fprintf(fp, "%f ", c[i * n + j]);
-	}
+        if(argc > 2)
+        {
+                for(i = 0; i < n; i++)
+                        for(j = 0; j < n; j++)
+                                fprintf(fp, "%f ", c[i * n + j]);
+        }
 
         return(0);
 }
---
+++ Asgn2/matrix_mul_intrinsic/myjob.cmd
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+#SBATCH -o /home/hpc/t1221/lu26xum/workspace/hpc-lab-ws13/Asgn2/matrix_mul_intrinsic/output_myjob.%j.%N.out
+#SBATCH -D /home/hpc/t1221/lu26xum/workspace/hpc-lab-ws13/Asgn2/matrix_mul_intrinsic
+#SBATCH -J mmintrin
+BATCH --get-user-env
+#SBATCH --partition=snb
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=4
+BATCH --mail-type=end
+#SBATCH --mail-user=gmkukreja@gmail.com
+#SBATCH --export=NONE
+#SBATCH --time=08:00:00
+
+source /etc/profile.d/modules.sh
+cd /home/hpc/t1221/lu26xum/workspace/hpc-lab-ws13/Asgn2/matrix_mul_intrinsic
+
+make run_test
