a9772049c2a050e1112ac12f8e180be9c59a73e0
Gaurav Kukreja <gaurav.kukreja@tum.de>
2013-11-24 03:05:19 +0100

    Tree Broadcast
    Signed-off-by: Gaurav Kukreja <gaurav.kukreja@tum.de>

---
+++ Asgn3/broadcast/tree_broadcast.c
@@ -0,0 +1,61 @@
+#include "mpi.h"
+#include <stdlib.h>
+#include <stdio.h>
+#include "timer.h"
+#include <math.h>
+
+#define N 1000
+
+int main(int argc, char **argv)
+{
+	int rank, size;
+	double *array;
+	int i;
+	MPI_Status status;
+	time_marker_t time;
+	double time_taken;
+	int receiver;
+
+	MPI_Init (&argc, &argv);
+	MPI_Comm_rank (MPI_COMM_WORLD, &rank);
+	MPI_Comm_size (MPI_COMM_WORLD, &size);
+
+	// printf("Rank = %d\n", rank);
+
+	array = (double*) malloc (sizeof(double) * N);
+
+	if(rank == 0)
+	{
+		for (i=0; i<N; i++)
+		{
+			array[i] = i;
+		}
+		time = get_time();
+	}
+	
+
+	for(i=0; i<ceil(log(size)/log(2)); i++)
+	{
+		if(rank < pow(2,i))
+		{
+			if(pow(2,i)+rank < size)
+			{
+				MPI_Send(array, N, MPI_DOUBLE_PRECISION, pow(2,i)+rank, 1, MPI_COMM_WORLD);
+				//printf("Thread %d sent to %d\n", rank, (int)pow(2,i)+rank);
+			}
+		}
+		if(rank >= pow(2,i) && rank <pow(2,i+1) && rank < size)
+		{
+			MPI_Recv(array, N, MPI_DOUBLE_PRECISION, rank-pow(2,i), 1, MPI_COMM_WORLD, &status);
+			//printf("Recvd\n");
+		}
+	}
+	if(rank == 0)
+	{
+		time_taken = get_ToD_diff_time (time);
+		printf("Thread(0) : Time taken = %e\n", time_taken);
+	}
+	
+	MPI_Finalize();
+	return 0;
+}
--- Asgn3/broadcast/trivial_broadcast.c
+++ Asgn3/broadcast/trivial_broadcast.c
@@ -1,8 +1,9 @@
 #include "mpi.h"
 #include <stdlib.h>
 #include <stdio.h>
+#include "timer.h"
 
-#define N 10
+#define N 1000
 
 int main(int argc, char **argv)
 {
@@ -10,12 +11,14 @@ int main(int argc, char **argv)
 	double *array;
 	int i;
 	MPI_Status status;
+	time_marker_t time;
+	double time_taken;
 
 	MPI_Init (&argc, &argv);
 	MPI_Comm_rank (MPI_COMM_WORLD, &rank);
 	MPI_Comm_size (MPI_COMM_WORLD, &size);
 
-	printf("Rank = %d\n", rank);
+	// printf("Rank = %d\n", rank);
 
 	array = (double*) malloc (sizeof(double) * N);
 
@@ -26,15 +29,18 @@ int main(int argc, char **argv)
 			array[i] = i;
 		}
 
+		time = get_time();
 		for (i=1; i<size; i++)
 		{
 			MPI_Send(array, N, MPI_DOUBLE_PRECISION, i, 1, MPI_COMM_WORLD);
 		}
+		time_taken = get_ToD_diff_time (time);
+		printf("Thread(0) : Time taken = %e\n", time_taken);
 	}
 	else
 	{
 		MPI_Recv(array, N, MPI_DOUBLE_PRECISION, 0, 1, MPI_COMM_WORLD, &status);
-		printf("Received by Thread = %d\n", rank);
+		// printf("Received by Thread = %d\n", rank);
 	}
 	
 	MPI_Finalize();
